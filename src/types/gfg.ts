3bd838618e5532e1dd33146e80361ddaa0647f70
Vinamra3215 <vinamragupta100@gmail.com>
2026-05-01 13:07:13 +0530

    feat(types): add geeksforgeeks type definitions with difficulty breakdown, streaks and institute

---
+++ src/types/gfg.ts
@@ -0,0 +1,15 @@
+export interface GFGUserStats {
+  username: string;
+  totalSolved: number;
+  codingScore: number;
+  institute: string;
+  instituteRank: number;
+  school: number;
+  basic: number;
+  easy: number;
+  medium: number;
+  hard: number;
+  currentStreak: number;
+  maxStreak: number;
+  contestRating: number;
+}
